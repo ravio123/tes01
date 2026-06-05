f4e77bcfe5b0a4419d5fd2579b268efc7dd29b10
ravio123 <aurovaravio@gmail.com>
2026-06-05 17:10:24 +0800

    Baru di tes

--- www/script.js
+++ www/script.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+function cekInternet() {
+
+    if(navigator.onLine){
+
+        document.getElementById("loadingScreen").style.display="none";
+        document.getElementById("appContent").style.display="block";
+
+    }else{
+
+        document.getElementById("loadingScreen").style.display="block";
+        document.getElementById("appContent").style.display="none";
+
+    }
+
+}
+
+window.addEventListener("online", cekInternet);
+window.addEventListener("offline", cekInternet);
+
+cekInternet();
